bf261fab77e9adf2acee547613c902b745ad9035
masterries <26522122+masterries@users.noreply.github.com>
2023-11-16 23:21:48 +0000

    Kleiner Fix dass auch git Message angezeigt wird

--- backend/backendapi.py
+++ backend/backendapi.py
@@ -11,11 +11,12 @@ def get_git_commit_info():
     try:
         commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
         commit_date = subprocess.check_output(["git", "log", "-1", "--format=%cd"]).decode().strip()
-        return commit_hash, commit_date
+        commit_message = subprocess.check_output(["git", "log", "-1", "--format=%B"]).decode().strip()
+        return commit_hash, commit_date, commit_message
     except Exception as e:
-        return str(e), None
+        return str(e), None, None
 
 @app.get("/git-info")
 def git_info():
-    commit_hash, commit_date = get_git_commit_info()
-    return {"commit_hash": commit_hash, "commit_date": commit_date}
+    commit_hash, commit_date, commit_message = get_git_commit_info()
+    return {"commit_hash": commit_hash, "commit_date": commit_date, "commit_message": commit_message}
