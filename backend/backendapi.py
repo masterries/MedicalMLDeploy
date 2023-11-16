bcbed7df128f4232d7208964a89fd65834c455cd
masterries <26522122+masterries@users.noreply.github.com>
2023-11-16 23:56:42 +0000

    test 4 action

--- backend/backendapi.py
+++ backend/backendapi.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Willkommen zur FastAPI-Anwendung! - 3"}
+    return {"message": "Willkommen zur FastAPI-Anwendung! - 4"}
 
 def get_git_commit_info():
     try:
