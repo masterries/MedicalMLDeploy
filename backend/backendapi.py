0c2b0685870ef014826d9bbd58a7d45b06139ba7
masterries <26522122+masterries@users.noreply.github.com>
2023-11-16 23:20:05 +0000

    adding git controll process

--- backend/backendapi.py
+++ backend/backendapi.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+import subprocess
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Willkommen zur FastAPI-Anwendung! - 2"}
+
+def get_git_commit_info():
+    try:
+        commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
+        commit_date = subprocess.check_output(["git", "log", "-1", "--format=%cd"]).decode().strip()
+        return commit_hash, commit_date
+    except Exception as e:
+        return str(e), None
+
+@app.get("/git-info")
+def git_info():
+    commit_hash, commit_date = get_git_commit_info()
+    return {"commit_hash": commit_hash, "commit_date": commit_date}
